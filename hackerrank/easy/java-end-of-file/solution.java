c0bc5d71d5b45c0d1785e447afd02a99707c553c
Subhranshu Sekhar Parida <125890429+Subhranshuu@users.noreply.github.com>
2026-09-21 16:43:44 +0530

    ✅ Java End-of-file | Easy | Java | Runtime: N/A | Memory: N/A | 2026-09-21

---
+++ hackerrank/easy/java-end-of-file/solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lineNumber = 1;
+        
+        // Continuously read until End-Of-File (EOF)
+        while (scanner.hasNext()) {
+            String line = scanner.nextLine();
+            System.out.println(lineNumber + " " + line);
+            lineNumber++;
+        }
+        
+        scanner.close();
+    }
+}
